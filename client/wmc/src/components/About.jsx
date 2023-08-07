1d31840fae3706df2ac55178a4821012d1e079ef
Sankalp-sa <sankalp.p@ahduni.edu.in>
2023-08-07 23:21:09 +0530

    Added about section to the page

--- client/wmc/src/components/About.jsx
+++ client/wmc/src/components/About.jsx
@@ -1,11 +1,36 @@
 import React from 'react'
+import Navbar from './Navbar'
 
 export default function About() {
   return (
-    <div id='aboutus'>
-      <h1 id="abthead">About us</h1>
-      <video id='ci' src="/castleintro2.mp4" autoPlay></video>
-      
+    <>
+    <Navbar />
+    <div id='aboutus' className='d-flex align-items-center justify-content-center flex-column'>
+      <h1 id="abthead" className='py-5 mb-5'>About us</h1>
+      {/* <video id='ci' src="/castleintro2.mp4" autoPlay></video> */}
+      <div className="row w-75">
+        <div class="container col-md-6 mb-1">
+          <div class="thecard d-flex align-items-center justify-content-center mb-5">
+            <div class="thefront skeleton d-flex align-items-center justify-content-center">
+              <img className='img-front' src='/SankalpPic.png' height={432} width={274} />
+            </div>
+            <div class="theback">
+              <img src='/SabkalpDesc.png' height={432} width={274} />
+            </div>
+          </div>
+        </div>
+        <div class="container col-md-6 mb-1">
+          <div class="thecard d-flex align-items-center justify-content-center mb-5">
+            <div class="thefront skeleton d-flex align-items-center justify-content-center">
+              <img className='img-front' src='/PriyanshPic.png' height={432} width={274} />
+            </div>
+            <div class="theback">
+              <img src='/PriyanshDesc.png' height={432} width={274} />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+    </>
   )
 }
