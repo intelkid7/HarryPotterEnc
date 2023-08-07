a8dcb6d5c046a3e6d3216be80c68d15ca8fb3884
intelkid7 <priyanshjsyadav777@gmail.com>
2023-08-07 13:39:39 +0530

    update02

--- client/wmc/src/components/About.jsx
+++ client/wmc/src/components/About.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>
-      <h1>About us</h1>
+    <div id='aboutus'>
+      <h1 id="abthead" >About us</h1>
+      <video id='ci' src="/castleintro2.mp4" autoPlay></video>
     </div>
   )
 }
