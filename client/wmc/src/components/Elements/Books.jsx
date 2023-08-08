891661681a0dec083b9a20b4ea88b25f77884a8f
Sankalp-sa <sankalp.p@ahduni.edu.in>
2023-08-08 13:02:55 +0530

    ui changes to books

--- client/wmc/src/components/Elements/Books.jsx
+++ client/wmc/src/components/Elements/Books.jsx
@@ -20,27 +20,22 @@ export default function Books() {
     return (
         <div>
             <Navbar />
-            <div className="row">
-                <div className="col-md-2">
-                    <Sidebar />
-                </div>
-                <div className="col-md-10 text-light">
-                    <h1 className='mb-5'>Books</h1>
-                    {/* Display books in form of cards */}
-                    <div className="row">
-                        {books.map((book) => (
-                                <div key={book.id} className='col-md-4 mb-5'>
-                                    <div className="card" style={{ width: '20rem' }}>
-                                        <img src={book.image_url} className="card-img-top"  />
-                                        <div className="card-body">
-                                            <h5 className="card-title">{book.title}</h5>
-                                            <a href="#" className="btn btn-primary">Go somewhere</a>
-                                        </div>
-                                    </div>
+            <div className="text-light d-flex align-items-center justify-content-center flex-column">
+                <h1 className='mb-5 title text-center'>Books</h1>
+                {/* Display books in form of cards */}
+                <div className="row d-flex align-items-center justify-content-center">
+                    {books.map((book) => (
+                        <div key={book.id} className='col-md-4 mb-5 d-flex align-items-center justify-content-center'>
+                            <div className="card" style={{ width: '20rem' }}>
+                                <img src={book.image_url} className="card-img-top" />
+                                <div className="card-body">
+                                    <h5 className="card-title">{book.title}</h5>
+                                    {/* <a href="#" className="btn btn-primary">Go somewhere</a> */}
                                 </div>
-                            )
-                        )}
-                    </div>
+                            </div>
+                        </div>
+                    )
+                    )}
                 </div>
             </div>
         </div>
